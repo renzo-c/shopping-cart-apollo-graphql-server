04cf38e00e94d28d25aa74333a26fc196b5bffa1
Renzo Navarro <rcnavarrop@gmail.com>
2022-01-07 00:20:41 -0500

    Adds node types for server

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
-const express = require("express");
-// rest of the code remains same
+import express from 'express';
+
 const app = express();
 const PORT = 8000;
+
 app.get("/", (req, res) => res.send("Express + TypeScript Server"));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
